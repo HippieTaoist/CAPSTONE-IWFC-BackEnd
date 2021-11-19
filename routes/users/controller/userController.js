82bd4bcaceab7ed70914f8f5ede568d7e3672677
[HippieTaoist] <[shawn.m.kittel@gmail.com]>
2021-11-19 14:48:42 -0700

    connected to login user with succesfful login

--- routes/users/controller/userController.js
+++ routes/users/controller/userController.js
@@ -16,10 +16,24 @@ const errorHandler = require("../../utils/errorHandler/errorHandler");
 
 
 
-function getUsers(req, res) {
-    res.json({
-        message: "soemthing cvool"
-    })
+async function usersGet(req, res) {
+
+    try {
+        let payload = await User.find(req.body);
+
+        res.json({
+            message: "Successfully retrieved",
+            payload: payload,
+        });
+    } catch (err) {
+        res.status(500).json({
+            message: "Failed Fetching",
+            error: err
+        });
+    }
+    // res.json({
+    //     message: "everything is awesome! cvool"
+    // })
 
     // try {
     //     let payload = await User.find(req.body);
@@ -37,8 +51,104 @@ function getUsers(req, res) {
     // }
 }
 
+async function userCreate(req, res) {
+    const {
+        nameFirst,
+        nameLast,
+        username,
+        email,
+        password
+    } = req.body;
+
+    console.log(req.body);
+
+    try {
+        let salt = await bcrypt.genSalt(10);
+        let hashedPassword = await bcrypt.hash(password, salt);
+
+        const createdUser = new User({
+            nameFirst,
+            nameLast,
+            username,
+            email,
+            password: hashedPassword,
+        });
+
+        let savedUser = await createdUser.save();
+
+        res.json({
+            message: "Successful",
+            payload: savedUser
+        });
+    } catch (err) {
+        res
+            .status(500)
+            .json({
+                message: "Error in Creating User",
+                error: errorHandler(err)
+            });
+    }
+}
+
+async function userLogin(req, res) {
+    const {
+        email,
+        username,
+        password,
+    } = req.body;
+
+    try {
+
+        let userEmailFound = await User.findOne({
+            email: email,
+        })
+
+        console.log(userEmailFound);
+        // let foundUserUsername = await User.findOne({
+        //     username: username,
+        // })
+
+        if (!userEmailFound) {
+            return res.status(500).json({
+                message: "Error in Logging In User",
+                error: " Go Sign UP",
+            })
+        } else {
+            let passwordCompare = await bcrypt.compare(password, userEmailFound.password);
+            if (!passwordCompare) {
+                return res.status(500).json({
+                    message: "error",
+                    error: "Please check email and password",
+                });
+            } else {
+                let jwtToken = jwt.sign({
+                    email: userEmailFound.email,
+                    username: userEmailFound.username,
+
+                }, process.env.SECRET_KEY, {
+                    expiresIn: "2400h",
+                });
+                return res.json({
+                    message: "Success Tokenizing",
+                    payload: jwtToken,
+                })
+            }
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: "Login Error.. WHAT DID YOU DO!!!!",
+            error: err.message
+        })
+    }
+
+
+}
+
+// async function
 
 module.exports = {
-    getUsers,
+    usersGet,
+    userCreate,
+    userLogin,
 
 }
--- routes/users/usersRouter.js
+++ routes/users/usersRouter.js
@@ -2,10 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 const {
-  getUsers
+  usersGet,
+  userCreate,
+  userLogin,
 } = require('./controller/userController')
 
 /* GET users listing. */
-router.get('/', getUsers)
+router.get('/', usersGet)
+
+router.post('/create-user', userCreate)
+
+router.post('/login-user', userLogin)
 
 module.exports = router;
