c7b75efed4d10a22b0c36ae2aaf14ad5592b2665
[HippieTaoist] <[shawn.m.kittel@gmail.com]>
2021-11-21 14:43:08 -0700

    complete favor/unfavor switch

--- routes/cryptos/cryptosRouter.js
+++ routes/cryptos/cryptosRouter.js
@@ -7,13 +7,16 @@ const {
 
 const {
     cryptosGet,
-    cryptoCreate
+    cryptoCreate,
+    cryptoUpdate
 } = require('./controller/cryptoController')
 
 /* GET Cryptos Saved By Users */
 router.get('/', middlewareJwt, cryptosGet)
 
 router.post('/crypto-create', middlewareJwt, cryptoCreate)
+
+router.put('/crypto-update', middlewareJwt, cryptoUpdate)
 
 // function (req, res, next) {
 //     res.json({
