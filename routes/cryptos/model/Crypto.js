e788ad86220a1758fcea5ee16029ad6dad5ead71
hippietaoist <Shawn.M.Kittel@gmail.com>
2021-12-07 08:20:17 -0700

    corrected schema to match that of CoinMarketData, where we are pulling initial and continual dat from

--- routes/cryptos/model/Crypto.js
+++ routes/cryptos/model/Crypto.js
@@ -1,49 +1,108 @@
 mongoose = require("mongoose");
 
-const {
-    isAlpha,
-    isURL
-} = require("validator");
+const { isAlpha, isURL } = require("validator");
 
-const cryptoSchema = new mongoose.Schema({
-    logoImgSrc: {
-        type: String,
-        validate: [isURL, "Logo Img Src URL incorrectly formatted"]
+const cryptoSchema = new mongoose.Schema(
+  {
+    cmcId: {
+      type: Number,
     },
-    nameCreator: {
-        type: mongoose.Schema.ObjectId,
-        ref: 'user'
+    name: {
+      type: String,
+      validate: [isAlpha, "Letters Only"],
     },
-    nameCrypto: {
-        type: String,
-        validate: [isAlpha, "Letters Only"]
+    symbol: {
+      type: String,
     },
-    nameSymbol: {
-        type: String,
-        unique: true,
-        validate: [isAlpha, "Letters Only"]
+
+    category: {
+      type: String,
     },
+    slug: {
+      type: String,
+    },
+    logo: {
+      type: String,
+      validate: [isURL, "Logo Img Src URL incorrectly formatted"],
+    },
+    subreddit: {
+      type: String,
+    },
+    tags: {
+      type: Array,
+    },
+    tagNames: {
+      type: Array,
+    },
+    urls: {
+      website: {
+        type: Array,
+      },
+      twitter: {
+        type: Array,
+      },
+      message_board: {
+        type: Array,
+      },
+      chat: {
+        type: Array,
+      },
+      facebook: {
+        type: Array,
+      },
+      explorer: {
+        type: Array,
+      },
+      reddit: {
+        type: Array,
+      },
+      technical_doc: {
+        type: Array,
+      },
+      source_code: {
+        type: Array,
+      },
+      announcement: {
+        type: Array,
+      },
+    },
+
+    platform: { type: Array },
+    date_added: { type: Date },
+    twitter_username: { type: String },
+    date_launched: { type: Date },
+    contract_address: { type: Array },
+
     priceCurrent: {
-        type: Number,
+      type: Number,
     },
     website: {
-        type: String,
-        validate: [isURL, "Website URL incorrectly formatted"]
+      type: String,
+      validate: [isURL, "Website URL incorrectly formatted"],
     },
-    usersFavored: [{
+    usersFavored: [
+      {
         type: mongoose.Schema.ObjectId,
-        ref: "user"
-    }],
-    usersUnfavored: [{
+        ref: "user",
+      },
+    ],
+    usersUnfavored: [
+      {
         type: mongoose.Schema.ObjectId,
-        ref: "user"
-    }],
-    programsAffiliated: [{
+        ref: "user",
+      },
+    ],
+
+    programsAffiliated: [
+      {
         type: mongoose.Schema.ObjectId,
-        ref: "cryptoProgram"
-    }]
-}, {
+        ref: "cryptoProgram",
+      },
+    ],
+  },
+  {
     timestamps: true,
-})
+  }
+);
 
 module.exports = mongoose.model("crypto", cryptoSchema);
